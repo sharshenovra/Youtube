a158f8319f3f43d410a119da412828e6f0c0722a
sharshenovra <67783261+sharshenovra@users.noreply.github.com>
2022-01-15 10:53:49 +0600

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Ruslan Sharshenov on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
